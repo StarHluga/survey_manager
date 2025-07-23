52037231b85efec8c8289a4e6e4910418fc52190
StarHluga <letsekasetlaba@gmail.com>
2025-07-23 17:51:21 +0200

    modified models.py from using function based code to class based code

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'surveys',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
--- mysite/surveys/admin.py
+++ mysite/surveys/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Survey, Question, Response, Answer
+from .models import Survey, Question, Choice, Response, Answer
 # Register your models here.
 
 admin.site.register(Survey)
 admin.site.register(Question)
+admin.site.register(Choice)
 admin.site.register(Response)
 admin.site.register(Answer)
